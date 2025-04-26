8ffb35e6d9803dbb8fadfc2657d828706bbd5671
siriveeramalla <siriveeramalla4@gmail.com>
2025-04-26 12:12:32 +0530

    update settings.py

--- suhitha/settings.py
+++ suhitha/settings.py
@@ -18,7 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "sirisuhitha@12"
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*","suhitha.onrender.com"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://suhitha.onrender.com'
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
