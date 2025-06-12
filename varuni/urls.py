81d48cba354fac0ae598c8f6daf747b66b495212
siriveeramalla <siriveeramalla4@gmail.com>
2025-06-12 19:11:39 +0530

    changes

--- varuni/urls.py
+++ varuni/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+     path("healthz/", health_check),
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
